c4c236b61c37def7bef6405b9db603fc049fc5a4
hiteshgupta2198 <hiteshgupta2198@gmail.com>
2024-06-13 22:49:59 +0000

    chat added

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 import os
 import cohere
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Initialize Cohere client with API key from environment variable
 co = cohere.Client("Rx2DD4J8sBxsrhsoUPrNbVulsqWPlrQxbAntYvCL")
 
 app = Flask(__name__, template_folder='src')
 
-@app.route("/")
+# Initial chat history
+initial_chat_history = [
+    {"role": "USER", "message": "Who discovered gravity?"},
+    {"role": "CHATBOT", "message": "The man who is widely credited with discovering gravity is Sir Isaac Newton"},
+]
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    response = co.chat(
-        chat_history=[
-            {"role": "USER", "message": "Who discovered gravity?"},
-            {
-                "role": "CHATBOT",
-                "message": "The man who is widely credited with discovering gravity is Sir Isaac Newton",
-            },
-        ],
-        message="What year was he born?",
-        connectors=[{"id": "web-search"}],
-    )
+    if request.method == "POST":
+        user_message = request.form["user-input"]
 
-    # Convert response to JSON string
-    response_json = response.json()
-    # return send_file('src/index.html')
+        # Add user's message to chat history
+        initial_chat_history.append({"role": "USER", "message": user_message})
 
-    return render_template('index.html', response=response_json)
+        # Call Cohere API to get response
+        response = co.chat(
+            chat_history=initial_chat_history,
+            message=user_message,
+            connectors=[{"id": "web-search"}],
+        )
+
+        # Add bot's response to chat history
+        initial_chat_history.append({"role": "CHATBOT", "message": response["message"]})
+
+    return render_template('index.html', messages=initial_chat_history)
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+    # Get port from environment variable or use default 80
+    port = int(os.getenv("PORT", 80))
+    app.run(port=port)
 
 if __name__ == "__main__":
     main()
